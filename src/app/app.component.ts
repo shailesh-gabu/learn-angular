e0676622e790ce1ae3cf973e59e34ad10640cb6f
Shailesh_Gabu <shaileshgabu.dev@gmail.com>
2026-02-11 01:57:56 +0530

    Add dynamic skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,75 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { SkeletonComponent, SkeletonRow } from './skeleton/skeleton.component';
+import { UserSkeletonComponent } from './user-skeleton/user-skeleton.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [SkeletonComponent, UserSkeletonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  isLoading = true;
+
+  layout: SkeletonRow[] = [
+    {
+      colGap: '10px',
+      rowGap: '10px',
+      wrap: false,
+      align: 'flex-start',
+      justify: 'center',
+
+      items: [
+        // Col 1 = Circle
+        { width: '100px', height: '100px', circle: true },
+
+        // Col 2 = Inside 2 rows
+        {
+          direction: 'column',
+          gap: '10px',
+          width: '100%',
+          wrap: true,
+          children: [
+            {
+              direction: 'row',
+              wrap: false,
+              gap: '10px',
+              children: [
+                { width: '50px', height: '50px' },
+                { width: '50px', height: '50px' },
+                { width: '50px', height: '50px' },
+                { width: '50px', height: '50px' },
+              ],
+            },
+            { width: '50%', height: '18px' },
+            { width: '100%', height: '14px' },
+          ],
+        },
+
+        // Col 3
+        {
+          direction: 'column',
+          gap: '10px',
+          width: '100%',
+          children: [
+            { width: '100%', height: '18px' },
+            { width: '100%', height: '14px' },
+          ],
+        },
+      ],
+    },
+
+    // Row 2
+    {
+      colGap: '15px',
+      rowGap: '1px',
+      items: [
+        { width: '100%', height: '30px' },
+        { width: '100%', height: '60px' },
+      ],
+    },
+  ];
   //only accept string
   title: string = 'learn-angular';
 
